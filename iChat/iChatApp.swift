c26c79ceb6d47992a31d04bbba641dc76a26204b
Patrick Alves <patrickalvesarts@gmail.com>
2023-08-12 20:31:33 -0300

    Initial Commit

---
+++ iChat/iChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChatApp.swift
+//  iChat
+//
+//  Created by Patrick Alves on 12/08/23.
+//
+
+import SwiftUI
+
+@main
+struct iChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
