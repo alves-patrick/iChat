7add28fa39be6ee9ee0178eda93a51e11b3c99c2
Patrick Alves <patrickalvesarts@gmail.com>
2023-08-13 06:19:33 -0300

    SignInView Finalizada

---
+++ iChat/SignInViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignInViewModel.swift
+//  iChat
+//
+//  Created by Patrick Alves on 13/08/23.
+//
+
+import Foundation
---
+++ iChat/SignUpIView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpIView.swift
+//  iChat
+//
+//  Created by Patrick Alves on 13/08/23.
+//
+
+import SwiftUI
+
+struct SignUpIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpIView()
+}
